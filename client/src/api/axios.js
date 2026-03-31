0143bed73fb4612ab8412679de269206b6196cab
M Sanath Kumar <sanathkumar017@gmail.com>
2026-04-01 00:21:32 +0530

    prepare backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.MODE === 'production' ? '' : 'http://localhost:5000',
+  baseURL: 'https://taskmaster-c4xs.onrender.com',
+  withCredentials: true,
 });
 
 // Attach token to every request
